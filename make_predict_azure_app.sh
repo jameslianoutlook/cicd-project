d81fef73aedc411581482e78267c2717dd3b494c
Lian Shiying <shiying.lian@mastercard.com>
2020-10-18 17:16:49 +0800

    add predictions bash

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-service.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-ap.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
